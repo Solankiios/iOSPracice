de30bcbedd512773d94404d5a1208819ee5c4069
SPCRi07 <solankiparth115@gmail.com>
2023-03-29 22:58:41 +0530

    HomePage Added

---
+++ layoutPractice/layoutPractice/ViewControllers/HomePageVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomePageVC.swift
+//  layoutPractice
+//
+//  Created by Solanki on 27/03/23.
+//
+
+import UIKit
+
+class HomePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
